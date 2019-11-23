2b42fba2ede4c51b1643b3ba063320261b25f1b7
Alex Kerney <abk@mac.com>
2019-11-23 17:46:10 -0500

    INO changes to main CPP

--- src/gage-particle.cpp
+++ src/gage-particle.cpp
@@ -20,8 +20,8 @@
 #include "logging/serial_logger.h"
 #include "logging/ble_serial_logger.h"
 
-#include "sensor/grove_temp.h"
-#include "sensor/maxbotix_serial_distance.h"
+#include "temp_sensor.h"
+#include "distance_sensor.h"
 
 void setup();
 void loop();
@@ -32,41 +32,57 @@ char *WEBHOOK_NAME = "Ubidots";                // Webhook name that Ubidots list
 
 // SYSTEM_THREAD(ENABLED);
 
+// Initialize our key systems
+
+// Settings manager persists settings to the EEPROM
+// and exposes Particle.functions for changing them
 SettingManager setting_manager(DEFAULT_UBIDOTS_UPDATE_SECONDS);
 
+// Network manager helps manage Wifi networks on an Argon
 NetworkManager network_manager;
 
+// Now for the specific loggers that should be registered to the DataLog
 UbidotsLogger ubidots_logger(WEBHOOK_NAME, setting_manager.current_settings());
 SerialLogger serial_logger;
 BleUartLogger bluetooth_logger;
 
+// Finally our sensors
 GroveTempSensor tempSensor(DHTPIN);
 MaxbotixDistanceSensor distance;
 
 void setup()
 {
+  // We first add our loggers in setup, so they are avaliable
+  // when other instances run through their setup.
   DataLog.add_logger(ubidots_logger);
   DataLog.add_logger(serial_logger);
   DataLog.add_logger(bluetooth_logger);
   DataLog.setup();
 
+  // Run through common setups/
   setting_manager.setup();
   network_manager.setup();
 
   DataLog.log_message("Begin readings!");
 
+  // Finally setup our sensors
   tempSensor.setup();
   distance.setup();
 }
 
 void loop()
 {
+  // First we loop through system level management
   setting_manager.loop();
   network_manager.loop();
 
+  // Then the sensor loops run which allows them to
+  // register values with the DataLog before they run
   tempSensor.loop();
   distance.loop();
 
+  // DataLog updates settings of any loggers that may have changes,
+  // Then completes the loggers complete their own loops.
   DataLog.update_settings(setting_manager.current_settings());
   DataLog.loop();
 }
